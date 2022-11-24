ce8133ed78431a5392cd38180f0744d4dba120a5
Seun Oshitade <87016257+s-oshitade@users.noreply.github.com>
2022-11-24 14:20:51 -0700

    Initialize 04_06

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        var item = "Shirt";
+        var size = "M";
+        var price = 14.99;
+        var color = "Red";
+
+    }
+}
