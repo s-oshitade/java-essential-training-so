8bc479da20a0e93906ae6653526cad79e8492dce
Seun Oshitade <87016257+s-oshitade@users.noreply.github.com>
2022-11-24 14:21:21 -0700

    Initialize 04_04

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int result = 10 + 12;
+        String answer = "The answer is " + result;
+        System.out.println(answer);
+    }
+}
