4f049352e8da2beaa20a16c600f8cf765e7610b1
Seun Oshitade <87016257+s-oshitade@users.noreply.github.com>
2022-11-24 14:20:57 -0700

    Initialize 05_01

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        var scanner = new Scanner(System.in);
+        System.out.print("Enter a month number (1-12): ");
+        var monthNumber = scanner.nextInt();
+
+    }
+}
