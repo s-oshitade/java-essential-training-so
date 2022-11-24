be00150fa12ff6e234b57af7e70f5561c41910c4
Seun Oshitade <seun.oshitade@gmail.com>
2022-11-24 14:33:16 -0700

    Evaluate conditions with if-else

---
+++ src/com/company/ConditionsWithIfElse.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.time.LocalDateTime;
+
+public class ConditionsWithIfElse {
+    public static void main(String[] args) {
+
+//        var scanner = new Scanner(System.in);
+//        System.out.print("Enter a month number (1-12): ");
+//        var monthNumber = scanner.nextInt();
+
+        var now = LocalDateTime.now();
+        var monthNumber = now.getMonthValue();
+
+        String message;
+//        if (monthNumber < 1 || monthNumber > 12) {
+//            message = "That isn't a valid month!";
+//        } else
+        if (monthNumber <= 3) {
+            message = "That's in Quarter 1!";
+        } else if (monthNumber <= 6) {
+            message = "That's in Quarter 2!";
+        } else {
+            message = "That isn't the first half of the year";
+        }
+        System.out.println(message);
+    }
+}
