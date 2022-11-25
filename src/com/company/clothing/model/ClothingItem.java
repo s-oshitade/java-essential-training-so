cf48c9d68e5880417d335431dadfa8b0fc394561
Seun Oshitade <seun.oshitade@gmail.com>
2022-11-25 08:32:00 -0700

    Use static fields as constants

--- src/com/company/clothing/model/ClothingItem.java
+++ src/com/company/clothing/model/ClothingItem.java
@@ -1,6 +1,11 @@
 package com.company.clothing.model;
 
 public class ClothingItem {
+    public static final String SHIRT = "Shirt";
+    public static final String PANTS = "Pants";
+    public static final String SHOES = "Shoes";
+
+
     private String type;
     private String size;
     private double price;
@@ -9,6 +14,13 @@ public class ClothingItem {
 //    public ClothingItem() {
 //    }
 
+
+//    public ClothingItem(String type, String size, double price, int quantity) {
+//        this.type = type;
+//        this.size = size;
+//        this.price = price;
+//        this.quantity = quantity;
+//    }
 
     public ClothingItem(String type, String size, double price, int quantity) {
         this.type = type;
