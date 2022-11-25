7c81e78cabbb1e8663b01673ea0d7eb6e995d56a
Seun Oshitade <seun.oshitade@gmail.com>
2022-11-25 01:40:13 -0700

    Close objects with try-with-resources

---
+++ src/com/company/CloseObjectsWithTryWithResources.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.io.*;
+
+public class CloseObjectsWithTryWithResources {
+
+    public static void main(String[] args) {
+        var file = new File("hello.txt");
+        System.out.println("File exists: " + file.exists());
+
+        try (FileReader reader = new FileReader(file);
+             BufferedReader br = new BufferedReader(reader)) {
+            var text = br.readLine();
+            System.out.println(text);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
