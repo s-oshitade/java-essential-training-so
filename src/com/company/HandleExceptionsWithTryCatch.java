696c38d9e1dfb6d0708ea6cb82bdd6081dc45e7a
Seun Oshitade <seun.oshitade@gmail.com>
2022-11-25 01:31:04 -0700

    Handle exceptions with try-catch

---
+++ src/com/company/HandleExceptionsWithTryCatch.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class HandleExceptionsWithTryCatch {
+
+    public static void main(String[] args) {
+
+        String s = null;
+        try {
+            var sub = s.substring(1);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("I'm not dead yet!");
+    }
+}
